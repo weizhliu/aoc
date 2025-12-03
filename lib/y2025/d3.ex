316739b47c39e24e9225ff8fc8f4aade0199af22
Weizheng Liu <60593974+weizhliu@users.noreply.github.com>
2025-12-04 00:11:52 +0800

    2025 d3 part2

--- lib/y2025/d3.ex
+++ lib/y2025/d3.ex
@@ -29,5 +29,26 @@ defmodule Y2025.D3 do
 
   def part2(input) do
     input
+    |> parse_to_lines()
+    |> Enum.map(fn line ->
+      line
+      |> search2()
+      |> Enum.join()
+      |> String.to_integer()
+    end)
+    |> Enum.sum()
   end
+
+  # "Remove K Digits" algorithm (solved with LLM assistance)
+  # Greedy + monotonic stack: remove k digits to maximize result
+
+  def search2(line) do
+    k = length(line) - 12
+    search2(line, [], k) |> Enum.reverse() |> Enum.take(12)
+  end
+
+  def search2([], stack, _k), do: stack
+  def search2([h | t], [], k), do: search2(t, [h], k)
+  def search2([h | t], [top | rest], k) when h > top and k > 0, do: search2([h | t], rest, k - 1)
+  def search2([h | t], stack, k), do: search2(t, [h | stack], k)
 end
